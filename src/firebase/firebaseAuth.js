17a93884f5a7abd72916d09c8407b270eb525ab4
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-09-22 16:31:39 +0100

    Add functions to setup authentification for boht email/password users and Google account users for user and staff login

---
+++ src/firebase/firebaseAuth.js
@@ -0,0 +1,38 @@
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { auth } from "./firebaseConfig";
+
+const googleLogin = async () => {
+  const provider = new GoogleAuthProvider();
+  try {
+    const outcome = await signInWithPopup(auth, provider);
+    const user = outcome.user;
+    return user;
+  } catch (error) {
+    console.error("Google login error", error);
+    throw error;
+  }
+};
+
+const emailLogin = async (email, password) => {
+  try {
+    const userCredentials = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return userCredentials.user;
+  } catch (error) {
+    console.error(
+      "error whilst signing in with email and password. Please try again",
+      error
+    );
+    throw error;
+  }
+};
+
+export { googleLogin, emailLogin };
