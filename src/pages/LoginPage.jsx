97e96854ae7efad1d806fae5dd5046c32fd63548
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-09-24 16:14:10 +0100

    LoginForm component passed onto LoginPage to render the form and also created event pages for both user and staff to test redirection of user based on their role data

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "../components/LoginForm.jsx";
+
+const LoginPage = () => {
+  return (
+    <section>
+      <h1>Welcome to EVNT5 platform</h1>
+      <LoginForm />
+    </section>
+  );
+};
+
+export default LoginPage;
---
+++ src/pages/StaffEventPage.jsx
@@ -0,0 +1,5 @@
+const StaffEventPage = () => {
+  return <h1>welcom to staff page</h1>;
+};
+
+export default StaffEventPage;
