60b17f064599d24fd7f57c28abd331d7f17ccb0c
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-09-24 16:17:31 +0100

    Import Routes, Route and pages to set up routing for in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-{
-  return <></>;
+import LoginPage from "./pages/LoginPage";
+import UserEventPage from "./pages/UserEventsPage";
+import StaffEventPage from "./pages/StaffEventPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/dashboard" element={<UserEventPage />} />
+      <Route path="/staff-dashboard" element={<StaffEventPage />} />
+    </Routes>
+  );
 }
 
 export default App;
