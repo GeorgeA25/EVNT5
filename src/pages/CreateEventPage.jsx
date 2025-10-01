ba843306e84f916a16deb39a09ab02f7dc4af6f0
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-01 15:53:43 +0100

    Added CreateEventPage which renders the EventForm component onto page for event creation

---
+++ src/pages/CreateEventPage.jsx
@@ -0,0 +1,12 @@
+import EventForm from "../components/CreateEventForm";
+
+const CreateEventPage = () => {
+  return (
+    <section>
+      <h1>Create New Event</h1>
+      <EventForm />
+    </section>
+  );
+};
+
+export default CreateEventPage;
