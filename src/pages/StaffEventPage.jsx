4b4676b88fa83f99c46f79abbe7347edc34efa99
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-01 15:51:21 +0100

    Add StaffEventPage function that fetches and displays events created based on the logged in staff members ID with error and load handling states

--- src/pages/StaffEventPage.jsx
+++ src/pages/StaffEventPage.jsx
@@ -1,5 +1,67 @@
+import { useState, useEffect } from "react";
+import {
+  getUserFromFirestore,
+  getEventsByStaffId,
+} from "../firebase/firebaseStore";
+import { auth } from "../firebase/firebaseConfig";
+import { useNavigate } from "react-router-dom";
+
 const StaffEventPage = () => {
-  return <h1>welcom to staff page</h1>;
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const currentUser = auth.currentUser;
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      if (!currentUser) {
+        setError("User not authenticated");
+        return;
+      }
+      setLoading(true);
+      setError(null);
+      try {
+        const eventList = await getEventsByStaffId(currentUser.uid);
+        setEvents(eventList);
+      } catch (error) {
+        console.error("Error whilst fetching events", error);
+        setError("Failed to load events onto page");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchEvents();
+  }, []);
+
+  const handleCreateEvent = () => {
+    navigate("/create-event");
+  };
+
+  const handleEventClick = (eventId) => {
+    navigate(`/events/${eventId}`);
+  };
+
+  return (
+    <section>
+      <h1>Welcome to the Staff Dashbaord</h1>
+      <button onClick={handleCreateEvent}>Create New Event</button>
+      {loading && <p>Loading evnt5...</p>}
+      {error && <p>{error}</p>}
+      {!loading && !error && events.length > 0 && (
+        <ul>
+          {events.map((event) => (
+            <li key={event.id} onClick={() => handleEventClick(event.id)}>
+              <h2>Title: {event.title}</h2>
+              <p>Date: {event.date}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 };
 
 export default StaffEventPage;
