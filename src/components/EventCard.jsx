64e4ac73c6d9549d79bcbd814d778a2fda85f4f6
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-02 15:21:54 +0100

    Create EventCard component to format and display event details onto page and navigate to event details page based on the event card thats been clicked on

---
+++ src/components/EventCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/EventCard.css";
+
+const EventCard = ({ event }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/events/${event.id}`);
+  };
+
+  return (
+    <div className="event-card" onClick={handleClick}>
+      <h3>{event.title}</h3>
+      <p>Description: {event.description}</p>
+      <p>Loaction: {event.description}</p>
+      <p>Type: {event.type}</p>
+      <p>Date: {event.date}</p>
+      <p>Startime: {event.startTime}</p>
+      <p>Endtime: {event.endTime}</p>
+      <p>Price: {event.price}</p>
+    </div>
+  );
+};
+
+export default EventCard;
