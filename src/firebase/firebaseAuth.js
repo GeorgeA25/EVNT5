17bf5d12c28eee5a16a3d8daad298406c66ff81c
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-14 19:26:28 +0100

    Refactor: provided parameters to enable refresh toekns for loger access duration to web app and pass in scope for oogle Calendar access. Add a connect function to handle oauth flow and to save both access/refresh tokens to firestore and include error handling and logging for debugging

--- src/firebase/firebaseAuth.js
+++ src/firebase/firebaseAuth.js
@@ -6,17 +6,72 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import { auth } from "./firebaseConfig";
+import {
+  addGoogleRefreshTokenToFirestore,
+  addGoogleUsersToFirestore,
+} from "./firebaseStore";
 
 const googleLogin = async () => {
+  console.log("googleLogin function called");
   const provider = new GoogleAuthProvider();
+  provider.setCustomParameters({
+    prompt: "consent",
+    access_type: "offline",
+  });
+  provider.addScope("https://www.googleapis.com/auth/calendar");
+
+  try {
+    console.log("Attempting to open Google sign-in popup...");
+    const outcome = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(outcome);
+    console.log(outcome);
+    await addGoogleRefreshTokenToFirestore(
+      outcome.user.uid,
+      outcome.user.refreshToken
+    );
+    await addGoogleUsersToFirestore(
+      outcome.user,
+      outcome._tokenResponse?.idToken || null,
+      credential?.accessToken || null
+    );
+
+    return outcome.user;
+  } catch (error) {
+    console.error("Google login error", error);
+    throw error;
+  }
+};
+
+const connectGoogleCalendar = async () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  provider.setCustomParameters({
+    prompt: "consent",
+    access_type: "offline",
+  });
+  provider.addScope(
+    `https://evnt5-97cf1.firebaseapp.com/oauth2callback?state=${uid}`
+  );
   try {
     const outcome = await signInWithPopup(auth, provider);
     const credential = GoogleAuthProvider.credentialFromResult(outcome);
-    const token = credential.accessToken;
-    const user = outcome.user;
-    return user;
+    const refreshToken =
+      credential?.refreshToken ||
+      outcome.user?.stsTokenManager?.refreshToken ||
+      null;
+
+    const accessToken = credential?.accessToken;
+
+    console.log("Google credential:", credential);
+    console.log("Access token:", accessToken);
+    console.log("Refresh token:", refreshToken);
+    if (refreshToken) {
+      await addGoogleRefreshTokenToFirestore(outcome.user.uid, refreshToken);
+    }
+
+    return { accessToken, refreshToken };
   } catch (error) {
-    console.error("Google login error", error);
+    console.error("error conectng to google calendar", error);
     throw error;
   }
 };
@@ -52,4 +107,4 @@ const registerUser = async (email, password) => {
   }
 };
 
-export { googleLogin, emailLogin, registerUser };
+export { googleLogin, emailLogin, registerUser, connectGoogleCalendar };
