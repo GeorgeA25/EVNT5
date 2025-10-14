b31be92d9b4d8ac1e44d87c8d9b0b9772454d207
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-14 20:03:35 +0100

    Add a handle function to handle 404 request gracefully and track current user to navigate them back to designated pages based on their email data

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase/firebaseConfig";
+
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+  const handleNotFoundPage = () => {
+    const currentUser = auth.currentUser;
+
+    if (!currentUser) {
+      navigate("/events");
+      return;
+    }
+
+    if (currentUser.email.includes("@evnt5.com")) {
+      navigate("/staff-dashboard");
+    } else {
+      navigate("/events");
+    }
+  };
+  return (
+    <section>
+      <p>
+        Oops this page doesn't exist. Please click the button to take you back
+        to the home page
+      </p>
+      <button onClick={handleNotFoundPage}>Home Page</button>
+    </section>
+  );
+};
+
+export default NotFoundPage;
