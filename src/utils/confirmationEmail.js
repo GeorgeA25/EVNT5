0a07c3c1e6dd98cda7a3a334dbb2f16d806bbac4
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-15 19:04:53 +0100

    Refactor: Pass in correct syntax for service, template and public to allow frontend to retrieve the data needed as failed on first implementation

--- src/utils/confirmationEmail.js
+++ src/utils/confirmationEmail.js
@@ -1,8 +1,8 @@
 import emailjs from "@emailjs/browser";
 
-const serviceId = import.meta.VITE_EMAILJS_SERVICE_ID;
-const templateId = import.meta.VITE_EMAILJS_TEMPLATE_ID;
-const publicKey = import.meta.VITE_EMAILJS_PUBLIC_KEY;
+const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
 const sendConfirmationEmail = async ({
   toEmail,
