dbc5bea0630c4ea8605b254ca9657c35a90caafc
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-16 20:15:59 +0100

    Update handleSignUp function with an if statement to track whether a current events price is a paid event, if so it'll navigate the user to the payment page

---
+++ src/pages/PaymentPage.jsx
@@ -0,0 +1,96 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  addPaymentInfoToFirestore,
+  addSignedUpEvents,
+} from "../firebase/firebaseStore";
+import { auth } from "../firebase/firebaseConfig";
+import sendConfirmationEmail from "../utils/confirmationEmail";
+import { useState } from "react";
+import PaymentForm from "../components/PaymentForm";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripeKey = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+
+const PaymentPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState("");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const {
+    eventDetails,
+    name: prefillName,
+    email: prefillEmail,
+  } = location.state || {};
+
+  const handlePayment = async (paymentIntent) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const currentUser = auth.currentUser;
+      if (!currentUser) {
+        setError("User must be logged in");
+        setLoading(false);
+        return;
+      }
+
+      const user = {
+        name: prefillName || currentUser.displayName,
+        email: prefillEmail || currentUser.email,
+      };
+
+      await addSignedUpEvents(user, eventDetails.id);
+
+      await sendConfirmationEmail({
+        toEmail: user.email,
+        name: user.name,
+        title: eventDetails.title,
+        date: eventDetails.date,
+        location: eventDetails.location,
+        startTime: eventDetails.startTime,
+        endTime: eventDetails.endTime,
+        time: new Date().toLocaleString(),
+      });
+
+      await addPaymentInfoToFirestore(
+        currentUser.uid,
+        eventDetails,
+        paymentIntent
+      );
+      setMessage(
+        "Payment successful! You are now signed up for the event. A confirmation email has been sent out."
+      );
+
+      setTimeout(() => {
+        navigate(`/events/${eventDetails.id}`);
+      }, 3000);
+    } catch (error) {
+      console.error(error);
+      setError(
+        "Payment succeeded but there was an error signing you up to the event",
+        error.message
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <h1>Pay for {eventDetails.title}</h1>
+      <p>Amount: £{Number(eventDetails.price.toFixed(2))}</p>
+      {error && <p role="alert">{error}</p>}
+      {message && <p role="status">{message}</p>}
+      {loading && <p>Processing...</p>}
+      <Elements stripe={stripeKey}>
+        <PaymentForm
+          amountPounds={Number(eventDetails.price)}
+          currency="gbp"
+          onPaymentSuccess={handlePayment}
+        />
+      </Elements>
+    </>
+  );
+};
+
+export default PaymentPage;
