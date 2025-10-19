0e5c8c571c7eed1e122da4488db0df2ab83a0361
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-19 10:13:56 +0100

    Pass a classname and add style for the whole application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
-//import "./App.css";
+import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import StaffEventPage from "./pages/StaffEventPage";
 import CreateEventPage from "./pages/CreateEventPage";
