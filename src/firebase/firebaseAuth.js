99038a043fa90983a05b2c909d11fbeb9624ad54
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-19 19:21:16 +0100

    Update url

--- src/firebase/firebaseAuth.js
+++ src/firebase/firebaseAuth.js
@@ -49,7 +49,9 @@ const connectGoogleCalendar = async () => {
     prompt: "consent",
     access_type: "offline",
   });
-  provider.addScope(`https://evnt5-97cf1.web.app/oauth2callback?state=${uid}`);
+  provider.addScope(
+    `https://evnt5-97cf1.firebaseapp.com/oauth2callback?state=${uid}`
+  );
   try {
     const outcome = await signInWithPopup(auth, provider);
     const credential = GoogleAuthProvider.credentialFromResult(outcome);
