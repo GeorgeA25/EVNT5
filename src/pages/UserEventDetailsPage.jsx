68ed94c46e2fda6d75dfbcd2a4ba54496229acfb
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-19 19:00:13 +0100

    Update localhost url to hosted url to redirect to hosted url

--- src/pages/UserEventDetailsPage.jsx
+++ src/pages/UserEventDetailsPage.jsx
@@ -221,7 +221,7 @@ const UserEventDetailsPage = () => {
     localStorage.setItem("pendingEventId", eventId);
     try {
       const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-      const redirectUri = "http://localhost:5173/oauth2callback";
+      const redirectUri = "https://evnt5-97cf1.web.app/oauth2callback";
       const scope = "https://www.googleapis.com/auth/calendar";
       const state = user.uid;
       const oauth2Url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(
