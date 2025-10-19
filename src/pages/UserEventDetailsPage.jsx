2d593dd9280c9d41f71dc57c0ae0fb7c16861103
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-10-19 19:22:23 +0100

    Update url

--- src/pages/UserEventDetailsPage.jsx
+++ src/pages/UserEventDetailsPage.jsx
@@ -221,7 +221,7 @@ const UserEventDetailsPage = () => {
     localStorage.setItem("pendingEventId", eventId);
     try {
       const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-      const redirectUri = "https://evnt5-97cf1.web.app/oauth2callback";
+      const redirectUri = "https://evnt5-97cf1.firebaseapp.com/oauth2callback";
       const scope = "https://www.googleapis.com/auth/calendar";
       const state = user.uid;
       const oauth2Url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(
@@ -407,13 +407,13 @@ const UserEventDetailsPage = () => {
                   or if you have a Google Account, sign up then add the event to
                   your Google Calendar, please visit this link.
                   <a
-                    href="https://evnt5-97cf1.web.app/"
+                    href="https://evnt5-97cf1.firebaseapp.com"
                     target="_blank"
                     rel="noopener noreferrer"
                     style={{ color: "blue", textDecoration: "underline" }}
                     className="event-shared-link"
                   >
-                    https://evnt5-97cf1.web.app/
+                    https://evnt5-97cf1.firebaseapp.com
                   </a>
                 </p>
               </div>
