7ad33b0801f1040b4ebc4930834dcd558e72cef5
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-11-20 18:36:53 +0000

    Updated component to add a new state for event type, generate eventTypes array by looping over events and an if statement so if events have a type then that events type gets pushed to new array. Then filter through events array dynamically based of the seledtType state and finally updated UI to show dropdown with all evenn types and all events option so user can view events based on event type

--- src/pages/UserEventsPage.jsx
+++ src/pages/UserEventsPage.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { getEventsFromFirestore } from "../firebase/firebaseStore";
-import { useNavigate } from "react-router-dom";
 import EventCard from "../components/EventCard";
 import UserNavbar from "../components/UserNavbar";
 import Footer from "../components/Footer";
@@ -11,6 +10,7 @@ const UserEventsPage = () => {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [selectedType, setSelectedType] = useState("All");
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -30,10 +30,34 @@ const UserEventsPage = () => {
     fetchEvents();
   }, []);
 
+  const eventTypes = [];
+  for (const event of events) {
+    if (!eventTypes.includes(event.type)) {
+      eventTypes.push(event.type);
+    }
+  }
+
+  const filteredEvents =
+    selectedType === "All"
+      ? events
+      : events.filter((event) => event.type === selectedType);
+
   return (
     <section className="events-section">
       <h1 className="events-message">Events Page</h1>
       <UserNavbar />
+      <select
+        value={selectedType}
+        onChange={(e) => setSelectedType(e.target.value)}
+        className="event-filter"
+      >
+        <option value="All">View All Event Types</option>
+        {eventTypes.map((type) => (
+          <option key={type} value={type} className="event-filter">
+            {type}
+          </option>
+        ))}
+      </select>
       {loading && (
         <p className="events-loading">Loading events please wait...</p>
       )}
@@ -43,7 +67,7 @@ const UserEventsPage = () => {
       )}
       {!loading && !error && events.length > 0 && (
         <div className="events-list">
-          {events.map((event) => (
+          {filteredEvents.map((event) => (
             <EventCard key={event.id} event={event} />
           ))}
         </div>
