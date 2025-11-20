e57f9389796fbe27457f4c774a4a1cdab17c8cfb
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-11-20 18:28:38 +0000

    Updated component by adding a useEffect to only display back to top button when users scroll a certain length down page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Footer = () => {
+  const [showButton, setShowButton] = useState(false);
   const handleScrollToTopOfPage = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
+
+    useEffect(() => {
+      const handleScroll = () => {
+        setShowButton(window.scrollY > 200);
+      };
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
   };
 
   return (
     <footer>
-      <button onClick={handleScrollToTopOfPage} className="footer-button">
-        Back to Top
-      </button>
+      {showButton && (
+        <button onClick={handleScrollToTopOfPage} className="footer-button">
+          Back to Top
+        </button>
+      )}
     </footer>
   );
 };
